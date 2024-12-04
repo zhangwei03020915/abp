d5013f22e4ffd1cd5a5526e741075b0216ea4b4f
liangshiwei <deawback@hotmail.com>
2024-12-04 13:57:13 +0800

    Update SwaggerHtmlResolver.cs

--- framework/src/Volo.Abp.Swashbuckle/Volo/Abp/Swashbuckle/SwaggerHtmlResolver.cs
+++ framework/src/Volo.Abp.Swashbuckle/Volo/Abp/Swashbuckle/SwaggerHtmlResolver.cs
@@ -11,7 +11,7 @@ public class SwaggerHtmlResolver : ISwaggerHtmlResolver, ITransientDependency
     public virtual Stream Resolver()
     {
         var scriptBundleScript = "<script src=\"%(ScriptBundlePath)\" charset=\"utf-8\"></script>";
-        var abpSwaggerScript = "<script src=\"/swagger/ui/abp.swagger.js\" charset=\"utf-8\"></script>";
+        var abpSwaggerScript = "<script src=\"ui/abp.swagger.js\" charset=\"utf-8\"></script>";
         var stream = typeof(SwaggerUIOptions).GetTypeInfo().Assembly
             .GetManifestResourceStream("Swashbuckle.AspNetCore.SwaggerUI.index.html");
 
