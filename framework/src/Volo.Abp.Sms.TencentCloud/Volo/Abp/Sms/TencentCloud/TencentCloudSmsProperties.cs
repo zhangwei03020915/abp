6064d08e28a6fd480ec2e46b82b93c0e7e869fee
Gary Jia <gary.gl.jia@outlook.com>
2024-12-16 23:02:29 +0800

    add tencent cloud sms

---
+++ framework/src/Volo.Abp.Sms.TencentCloud/Volo/Abp/Sms/TencentCloud/TencentCloudSmsProperties.cs
@@ -0,0 +1,7 @@
+﻿namespace Volo.Abp.Sms.TencentCloud;
+
+public static class TencentCloudSmsProperties
+{
+    public const string SignName = "SignName";
+    public const string TemplateId = "TemplateId";
+}
