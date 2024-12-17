9a3a281bb080a1404f49ef14f158ce24cd181b90
masum-ulu <masum.ulu@hotmail.com>
2024-12-17 14:38:58 +0300

    update: ng-packs > dev-app

--- npm/ng-packs/apps/dev-app/src/app/route.provider.ts
+++ npm/ng-packs/apps/dev-app/src/app/route.provider.ts
@@ -1,20 +1,20 @@
 import { eLayoutType, RoutesService } from '@abp/ng.core';
-import { APP_INITIALIZER } from '@angular/core';
+import { inject, provideAppInitializer } from '@angular/core';
 
 export const APP_ROUTE_PROVIDER = [
-  { provide: APP_INITIALIZER, useFactory: configureRoutes, deps: [RoutesService], multi: true },
+  provideAppInitializer(() => {
+    configureRoutes(inject(RoutesService));
+  }),
 ];
 
 function configureRoutes(routesService: RoutesService) {
-  return () => {
-    routesService.add([
-      {
-        path: '/',
-        name: '::Menu:Home',
-        iconClass: 'fas fa-home',
-        order: 1,
-        layout: eLayoutType.application,
-      },
-    ]);
-  };
+  routesService.add([
+    {
+      path: '/',
+      name: '::Menu:Home',
+      iconClass: 'fas fa-home',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+  ]);
 }
