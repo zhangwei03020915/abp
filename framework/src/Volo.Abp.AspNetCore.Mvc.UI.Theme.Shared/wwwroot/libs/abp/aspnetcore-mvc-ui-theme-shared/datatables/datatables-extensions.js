c6ad659f769d6ce47ec912e79c3e45f2cddbdb68
maliming <malimings@gmail.com>
2024-12-19 17:55:21 +0800

    Update datatables-extensions.js

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/datatables/datatables-extensions.js
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/datatables/datatables-extensions.js
@@ -87,7 +87,7 @@ var abp = abp || {};
             if (field.items.length === 1) {
                 var firstItem = field.items[0];
                 if (!getVisibilityValue(firstItem.visible, record, tableInstance)) {
-                    return $('');;
+                    return $('');
                 }
 
                 var $button = $('<button type="button" class="btn btn-primary btn-sm abp-action-button"></button>');
